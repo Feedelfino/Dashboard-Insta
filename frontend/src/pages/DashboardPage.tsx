2ea020e3c4f4e30e3aa908f65bfc342945bb69f9
Felipe Delfino <felipemoreraidelfino@gmail.com>
2026-05-04 22:44:10 -0300

    feat: adicionei dashboard com cards e gráficos de métricas

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { Navigate } from 'react-router-dom'
+import {LineChart,Line,XAxis,YAxis,Tooltip,CartesianGrid,} from 'recharts'
 
 // Tipo da métrica recebida do backend
 type Metric = {
@@ -15,6 +16,7 @@ function DashboardPage() {
 
   const [metric, setMetric] = useState<Metric | null>(null)
   const [loading, setLoading] = useState(true)
+  const [historico, setHistorico] = useState<Metric[]>([])
 
   useEffect(() => {
     async function buscarMetricas() {
@@ -35,6 +37,18 @@ function DashboardPage() {
         }
 
         setMetric(dados)
+
+     // Busca a lista para por no gráfico   
+        const respostaHistorico = await fetch('http://127.0.0.1:8000/metrics/historico', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+     })
+     
+     
+      const dadosHistorico = await respostaHistorico.json()
+        
+        setHistorico(dadosHistorico)
 
       } catch (error) {
         console.error(error)
@@ -63,15 +77,60 @@ function DashboardPage() {
       )}
 
       {!loading && metric && (
-        <div>
-          <p>Seguidores: {metric.seguidores}</p>
-          <p>Alcance: {metric.alcance}</p>
-          <p>Impressões: {metric.impressoes}</p>
-          <p>Atualizado em: {metric.criado_em}</p>
+        <div style={styles.grid}>
+          <div style={styles.card}>
+            <h3>seguidores</h3>
+            <p>{metric.seguidores}</p>
+        </div>
+
+        <div style={styles.card}>
+            <h3>Alcance</h3>
+            <p>{metric.alcance}</p>
+        </div>
+
+        <div style={styles.card}>
+          <h3>Impressões</h3>
+          <p>{metric.impressoes}</p>
+        </div>
         </div>
       )}
-    </div>
+
+      {historico.length >0 && (
+        <div style={styles.chartBox}>
+          <h2>Evolução de Seguidores</h2>
+
+          <LineChart width={600} height={300} data={historico}>
+            <CartesianGrid stroke='#ccc' />
+            <XAxis dataKey="criado_em" />
+            <YAxis />
+            <Tooltip/>
+            <Line type="monotone" dataKey="seguidores" stroke="#2563eb"/>
+          </LineChart>
+        </div>
+      )}
+    </div>  
   )
+}
+
+
+const styles = {
+  grid: {
+    display: 'flex',
+    gap: '20px',
+    marginTop: '20px',
+  },
+
+  card: {
+    background: '#ffffff',
+    padding: '20px',
+    borderRadius: '12px',
+    boxShadow: '0 4px 12px rgba(0,0,0,0.08)',
+    minWidth: '150px',
+  },
+
+    chartBox: {
+      marginTop: '40px',
+  },
 }
 
 export default DashboardPage
