31f634d5440f8dfc6ab46eed6d39be06e6adc304
Felipe Delfino <felipemoreraidelfino@gmail.com>
2026-05-08 02:08:30 -0300

    feat: melhoras no UI do dashboard e login page

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -1,6 +1,13 @@
 import { useEffect, useState } from 'react'
 import { Navigate } from 'react-router-dom'
-import {LineChart,Line,XAxis,YAxis,Tooltip,CartesianGrid,} from 'recharts'
+import {LineChart,
+        Line,
+        XAxis,
+        YAxis,
+        Tooltip,
+        CartesianGrid,
+        ResponsiveContainer,
+      } from 'recharts'
 
 // Tipo da métrica recebida do backend
 type Metric = {
@@ -17,6 +24,10 @@ function DashboardPage() {
   const [metric, setMetric] = useState<Metric | null>(null)
   const [loading, setLoading] = useState(true)
   const [historico, setHistorico] = useState<Metric[]>([])
+
+  function formatarData(data: string) {
+    return new Date(data).toLocaleDateString('pt-BR')
+  }
 
   useEffect(() => {
     async function buscarMetricas() {
@@ -67,10 +78,23 @@ function DashboardPage() {
   }
 
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div style={styles.container}>
+    <div style={styles.header}>
+      <div>
+        <h1 style={styles.title}>Dashboard de Métricas</h1>
+        <p style={styles.subtitle}>
+          Acompanhe os principais indicadores do perfil em tempo real.
+          </p>
+      </div>
+     </div> 
 
-      {loading && <p>Carregando métricas...</p>}
+      {loading && (
+        <div style={styles.loadingBox}>
+          <p style={styles.loadingText}>
+            Carregando métricas...
+          </p>
+        </div>
+      )}
 
       {!loading && !metric && (
         <p>Nenhuma métrica encontrada.</p>
@@ -79,18 +103,18 @@ function DashboardPage() {
       {!loading && metric && (
         <div style={styles.grid}>
           <div style={styles.card}>
-            <h3>seguidores</h3>
-            <p>{metric.seguidores}</p>
+            <h3 style={styles.cardTitle}>seguidores</h3>
+            <p style={styles.cardValue}>{metric.seguidores}</p>
         </div>
 
         <div style={styles.card}>
-            <h3>Alcance</h3>
-            <p>{metric.alcance}</p>
+            <h3 style={styles.cardTitle}>Alcance</h3>
+            <p style={styles.cardValue}>{metric.alcance}</p>
         </div>
 
         <div style={styles.card}>
-          <h3>Impressões</h3>
-          <p>{metric.impressoes}</p>
+          <h3 style={styles.cardTitle}>Impressões</h3>
+          <p style={styles.cardValue}>{metric.impressoes}</p>
         </div>
         </div>
       )}
@@ -99,13 +123,20 @@ function DashboardPage() {
         <div style={styles.chartBox}>
           <h2>Evolução de Seguidores</h2>
 
-          <LineChart width={600} height={300} data={historico}>
-            <CartesianGrid stroke='#ccc' />
-            <XAxis dataKey="criado_em" />
-            <YAxis />
-            <Tooltip/>
-            <Line type="monotone" dataKey="seguidores" stroke="#2563eb"/>
-          </LineChart>
+          <ResponsiveContainer width="100%" height={300}>
+            <LineChart data={historico}>
+              <CartesianGrid stroke="#ccc" />
+              <XAxis 
+              dataKey="criado_em"
+              tickFormatter={formatarData} />
+              <YAxis />
+              <Tooltip/>
+              <Line 
+                type="monotone" 
+                dataKey="seguidores" 
+                stroke="#2563eb"/>
+            </LineChart>
+          </ResponsiveContainer>
         </div>
       )}
     </div>  
@@ -114,10 +145,57 @@ function DashboardPage() {
 
 
 const styles = {
+  
+  loadingBox: {
+    marginTop: '20px',
+    padding: '20px',
+    background: '#ffffff',
+    borderRadius: '12px',
+    boxShadow: '0 4px 12px rgba(0,0,0,0.08)',
+  },
+
+  loadingText: {
+    margin:0,
+    color: '#666',
+  },
+
+  container: {
+    padding: '40px',
+    maxWidth: '1000px',
+    margin: '0 auto',
+  },
+
+  header: {
+    marginBottom: '30px',
+  },
+
+  title: {
+    fontSize: '32px',
+    marginBotton: '8px',
+  },
+
+  subtitle: {
+    color: '#666',
+    fontSize: '16px',
+    margin: 0,
+  },
+
   grid: {
     display: 'flex',
     gap: '20px',
     marginTop: '20px',
+  },
+
+  cardTitle: {
+    fontSize: '14px',
+    color: '#666',
+    margin: 0
+  },
+
+  cardValue: {
+    fontSize: '32px',
+    fontWeigth: 'bold' as const,
+    margin: 0,
   },
 
   card: {
@@ -126,10 +204,17 @@ const styles = {
     borderRadius: '12px',
     boxShadow: '0 4px 12px rgba(0,0,0,0.08)',
     minWidth: '150px',
+    display: 'flex',
+    flexDirection: 'column'as const,
+    gap: '10px',
   },
 
-    chartBox: {
-      marginTop: '40px',
+  chartBox: {
+    marginTop:'40px',
+    background: '#ffffff',
+    padding: '24px',
+    borderRadius: '12px',
+    boxShadow: '0 4px 12px rgba(0,0,0,0.08)',
   },
 }
 
